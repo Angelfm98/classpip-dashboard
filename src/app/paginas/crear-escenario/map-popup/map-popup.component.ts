55667542425bbaea4245b0a94090ce50ca2532d4
errezeeta <jramirezsamc@gmail.com>
2023-01-19 09:27:14 +0100

    commit 19/1

--- src/app/paginas/crear-escenario/map-popup/map-popup.component.ts
+++ src/app/paginas/crear-escenario/map-popup/map-popup.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PeticionesAPIService } from 'src/app/servicios';
+import { CrearEscenarioComponent } from '../crear-escenario.component';
+import { Location } from '@angular/common';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map-popup',
   templateUrl: './map-popup.component.html',
   styleUrls: ['./map-popup.component.scss']
 })
-export class MapPopupComponent implements OnInit {
+export class MapPopupComponent implements AfterViewInit {
 
-  constructor() { }
+  private map:any;
+  private selection: any;
 
-  ngOnInit() {
+  constructor(
+    public dialog: MatDialog,
+    public location: Location,
+    private peticionesAPI: PeticionesAPIService,
+    public dialogRef: MatDialogRef<MapPopupComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  onNoClick() {
+    this.dialogRef.close();
+  }
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
   }
 
 }
